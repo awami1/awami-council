118f97f443b20e9989b1add12b39fc7ea50399c3
awami1 <34949690+awami1@users.noreply.github.com>
2026-02-20 01:52:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
-const express = require(‘express’);
+const http = require(‘http’);
+const fs = require(‘fs’);
 const path = require(‘path’);
-const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Serve public website from root
-app.use(express.static(path.join(__dirname)));
+const mimeTypes = {
+‘.html’: ‘text/html’,
+‘.js’:   ‘application/javascript’,
+‘.css’:  ‘text/css’,
+‘.png’:  ‘image/png’,
+‘.jpg’:  ‘image/jpeg’,
+‘.svg’:  ‘image/svg+xml’,
+‘.json’: ‘application/json’,
+‘.ico’:  ‘image/x-icon’
+};
 
-// Serve index.html for root
-app.get(’/’, (req, res) => {
-res.sendFile(path.join(__dirname, ‘index.html’));
+const server = http.createServer((req, res) => {
+let filePath = path.join(__dirname, req.url === ‘/’ ? ‘index.html’ : req.url);
+
+fs.readFile(filePath, (err, data) => {
+if (err) {
+// Try index.html as fallback
+fs.readFile(path.join(__dirname, ‘index.html’), (err2, data2) => {
+if (err2) {
+res.writeHead(404);
+res.end(‘Not found’);
+} else {
+res.writeHead(200, { ‘Content-Type’: ‘text/html’ });
+res.end(data2);
+}
+});
+return;
+}
+const ext = path.extname(filePath);
+const contentType = mimeTypes[ext] || ‘application/octet-stream’;
+res.writeHead(200, { ‘Content-Type’: contentType });
+res.end(data);
+});
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 console.log(’Server running on port ’ + PORT);
 });
